8574dae4a66f703ba4b69c4562f28c482d39cb2b
azizandri <azizandri72@gmail.com>
2025-11-12 11:38:55 +0700

    Final Keyword

---
+++ src/week10/andri/id/ac/umn/a.java
@@ -0,0 +1,7 @@
+package week10.andri.id.ac.umn;
+
+final class a {
+	final void method1() {
+		System.out.println("This is final method.");
+	}
+}
---
+++ src/week10/andri/id/ac/umn/b.java
@@ -0,0 +1,8 @@
+package week10.andri.id.ac.umn;
+
+public class b extends a {
+	void method1() {
+		//Compile-error! we cannot override
+		System.out.println("Illegal");
+	}
+}
